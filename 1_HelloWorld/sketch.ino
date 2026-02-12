077dca0b9a9d1eca29b6c637022f0773100694c1
Ismail Hossain <ismail.hossain@centria.fi>
2026-02-12 21:23:40 +0200

    Add counter to Hello World sketch

--- 1_HelloWorld/sketch.ino
+++ 1_HelloWorld/sketch.ino
@@ -1,1 +1,18 @@
+// Exercise 1 - Hello World
 
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(counter);
+  counter++;
+
+  if(counter > 10) {
+    counter = 0;
+  }
+
+  delay(1000);
+}
